6c166bc6adbc969a7c02f04fc878e28bb6fac67f
Dillon Greek <techhead404@gmail.com>
2022-03-13 18:27:03 -0500

    Added Auth0 to create page.

--- app.js
+++ app.js
@@ -22,17 +22,37 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 //app.use(morgan('dev'));
+//////////////////////////////////////////
+const { auth, requiresAuth } = require('express-openid-connect');
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/login', (req, res) => {
+   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+ });
+//////////////////////////////////////////
 
 app.get('/',(req, res) => {
     res.render('index',{title:'Home'});
-});2
+});
 
 app.get('/about',(req, res) => {
     res.render('about',{title:'About'});
 });
 
-app.get('/blogs/create',(req, res) => {
+app.get('/blogs/create',requiresAuth() ,(req, res) => {
+    //res.send(JSON.stringify(req.oidc.user))
     res.render('create',{title:'Create'});
 });
 
@@ -71,7 +91,7 @@ app.get('/blogs/:id', (req,res) => {
     })
 });
 
-app.delete('/blogs/:id', (req, res)=>{
+app.delete('/blogs/:id',requiresAuth(), (req, res)=>{
     const id = req.params.id;
     Blog.findByIdAndDelete(id)
         .then(result => {
